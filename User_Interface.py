073c7bb9e2f0152a9de72a1e3d3f4757378b0a09
Anush <anushmhd@gmail.com>
2023-08-03 20:01:56 -0400

    Added Banner

---
+++ User_Interface.py
@@ -0,0 +1,14 @@
+def display_banner() -> None:
+    """
+    :rtype: None
+    """
+    banner = (r"" "\n"
+              r" ___         ___  __   ___      ___ " "\n"
+              r"|__  | |    |__  |__) |__   /\   |  " "\n"
+              r"|    | |___ |___ |__) |___ /~~\  |  " "\n"
+              r"                                    " "\n"
+              r"    ")
+    print(banner)
+
+
+display_banner()
