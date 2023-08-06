4fce2ce932b26b83d6b3dda5bb3390d14c65b71d
rahulchoudhary1839 <141481671+rahulchoudhary1839@users.noreply.github.com>
2023-08-05 21:30:14 -0400

    Integrity_verification Module
    Module added

---
+++ Integrity_verification.py
@@ -0,0 +1,53 @@
+import os
+import time
+import multiprocessing
+import Hashing
+import sys
+import User_Interface
+import colors
+
+def read_baseline_files(baseline_path):
+    data = {}
+    with open(baseline_path, 'r') as f:
+        for line in f:
+            path, hexdigest = line.strip().split("%")
+            data[path] = hexdigest
+    return data
+
+
+def integrity_monitoring(file_path, baseline_list):
+
+    while True:
+        for baseline_data in baseline_list:
+            for path, digest in baseline_data.items():
+                if not os.path.exists(path):
+                    print(f"ALERT: File at path '{path}' has been deleted or moved")
+                else:
+                    current_hash = Hashing.calculate_hash(path)
+                    if current_hash != digest:
+                        print(f"ALERT: File at path '{path}' has been modified.")
+        time.sleep(60)
+
+
+def main():
+    User_Interface.clrscr()
+    colors.print_red("Beginning monitoring of the drives\n")
+    path = sys.argv
+    path.pop(0)
+
+    baseline_paths = list(map(lambda x: "Baseline\\" + x + " baselines.txt", path))
+
+    baseline_list = [read_baseline_files(baselines) for baselines in baseline_paths]
+
+    pools = []
+    for i, filepath in enumerate(path):
+        pool = multiprocessing.Process(target=integrity_monitoring, args=(filepath, baseline_list))
+        pools.append(pool)
+        pool.start()
+
+    for pool in pools:
+        pool.join()
+
+
+if __name__ == "__main__":
+    main()
